5c121e9565799cc1ced5ee4c33c0496024407eff
natmulu <natnaelmulu16@gmail.com>
2025-04-04 21:14:10 +0300

    Basic Information and Apperance stepper.

--- src/views/Prisioner/PrisonerStepper.vue
+++ src/views/Prisioner/PrisonerStepper.vue
@@ -1,7 +1,7 @@
 <template>
   <AdminLayout>
     <PageBreadcrumb :pageTitle="currentPageTitle" />
-      <div class="flex items-center justify-center space-x-4">
+      <div class="flex items-center justify-center space-x-4 mb-10">
         <div v-for="(step, index) in steps" :key="step.no" class="flex items-center">
           <div 
             class="w-10 h-10 flex items-center justify-center rounded-full cursor-pointer"
@@ -14,9 +14,12 @@
           <div v-if="index < steps.length - 1" class="w-12 h-1 bg-gray-300" :class="currentStep > step.no ? 'bg-blue-600' : ''"></div>
         </div>
      </div>
-     <Prisioner v-if="currentStep == 0"> </Prisioner>
-     <PrisionerApperance v-if="currentStep == 1"> </PrisionerApperance>
+     <BasicInformation @prisonerSaved="changeStep(1)" v-if="currentStep == 0"> </BasicInformation>
+     <!-- <Prisioner v-if="currentStep == 1"> </Prisioner> -->
+     <PrisionerApperance @prisonerApperanceSaved="changeStep(2)" v-if="currentStep == 1"> </PrisionerApperance>
      <PrisionerProperty v-if="currentStep == 2"> </PrisionerProperty>
+
+
   </AdminLayout>
 </template>
 
@@ -28,10 +31,11 @@ import AdminLayout from '@/components/layout/AdminLayout.vue'
 import Prisioner from '@/views/Prisioner/Prisioner.vue'
 import PrisionerApperance from './PrisionerApperance.vue'
 import PrisionerProperty from './PrisionerProperty.vue'
+import BasicInformation from './Steps/BasicInformation.vue'
 
 export default {
   components: {
-    PageBreadcrumb, AdminLayout, Prisioner, PrisionerApperance, PrisionerProperty, 
+    PageBreadcrumb, AdminLayout, Prisioner, PrisionerApperance, PrisionerProperty, BasicInformation,
   },
   data() {
     return {
