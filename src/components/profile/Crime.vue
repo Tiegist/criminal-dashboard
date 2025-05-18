3d4fb965b1d21110cf499e6b6186bcaa957b1016
Tiegist Girma <tigistgirma40@gmail.com>
2025-05-18 20:37:52 +0300

    court history added

--- src/components/profile/Crime.vue
+++ src/components/profile/Crime.vue
@@ -6,35 +6,74 @@
           <h4 class="text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-6">Crime</h4>
 
           <div class="grid grid-cols-1 gap-4 lg:grid-cols-1 lg:gap-7 2xl:gap-x-32">
-
-            <div v-for="crime, i in props.history.prisioner_crimes" :key="i">
-              <div>
-                <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">{{ crime.crime.name }}</p>
-                <p class="text-sm font-medium text-gray-800 dark:text-white/90"> {{ crime.crime_description }} </p>
+            <div class="flex ">
+              <div
+                v-for="(crime, i) in history.prisioner_crimes"
+                :key="i"
+                class="min-w-[200px]"
+              >
+                <p class="mb-2 text-xs leading-normal text-gray-500 dark:text-gray-400">
+                  {{ crime.crime.name }}
+                </p>
+                <p class="text-sm font-medium text-gray-800 dark:text-white/90">
+                  {{ crime.crime_description }}
+                </p>
               </div>
+              <button v-if="prisoner?.id" @click="addNewStory(prisoner.id)"
+                 class="edit-button absolute right-20 mt-1">
+                <svg
+                  class="fill-current"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M15.0911 2.78206C14.2125 1.90338 12.7878 1.90338 11.9092 2.78206L4.57524 10.116C4.26682 10.4244 4.0547 10.8158 3.96468 11.2426L3.31231 14.3352C3.25997 14.5833 3.33653 14.841 3.51583 15.0203C3.69512 15.1996 3.95286 15.2761 4.20096 15.2238L7.29355 14.5714C7.72031 14.4814 8.11172 14.2693 8.42013 13.9609L15.7541 6.62695C16.6327 5.74827 16.6327 4.32365 15.7541 3.44497L15.0911 2.78206ZM12.9698 3.84272C13.2627 3.54982 13.7376 3.54982 14.0305 3.84272L14.6934 4.50563C14.9863 4.79852 14.9863 5.2734 14.6934 5.56629L14.044 6.21573L12.3204 4.49215L12.9698 3.84272ZM11.2597 5.55281L5.6359 11.1766C5.53309 11.2794 5.46238 11.4099 5.43238 11.5522L5.01758 13.5185L6.98394 13.1037C7.1262 13.0737 7.25666 13.003 7.35947 12.9002L12.9833 7.27639L11.2597 5.55281Z"
+                  />
+                </svg>
+                አዲስ የፍርድ ቤት ክትትል
+              </button>
             </div>
-
           </div>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import Modal from './Modal.vue'
+import { useRoute, useRouter } from 'vue-router'
+import { ref, onMounted } from 'vue'
 
-const isProfileAddressModal = ref(false)
+const route = useRoute()
+const router = useRouter()
 
-const props = defineProps(['history'])
+const prisoner = ref(null)
 
-const saveProfile = () => {
-  // Implement save profile logic here
-  console.log('Profile saved')
-  isProfileInfoModal.value = false
+onMounted(() => {
+  const prisonerId = route.query.prisoner
+  if (prisonerId) {
+    prisoner.value = { id: prisonerId }
+  }
+})
+
+const addNewStory = (prisoner_id) => {
+  router.push({ name: 'PrisionerCourtHistory', query: { prisoner: prisoner_id } })
 }
-</script>
+const props = defineProps({
+  history: {
+    type: Object,
+    default: () => ({ prisioner_crimes: [] })
+  },
+  prisoner: {
+    type: Object,
+    default: () => ({})
+  }
+})
 
-<style></style>
+
+</script>
