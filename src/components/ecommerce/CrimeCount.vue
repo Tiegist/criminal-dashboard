c768da1d1c588dce18a24c470b41d5487f00fc15
natmulu <natnaelmulu16@gmail.com>
2025-06-10 10:30:10 +0300

    Dashboard gender, crime, and monthly arrest.

--- src/components/ecommerce/CrimeCount.vue
+++ src/components/ecommerce/CrimeCount.vue
@@ -2,7 +2,7 @@
 	<div
 		class="overflow-hidden rounded-2xl border border-gray-200 bg-white px-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
 		<div class="flex items-center justify-between">
-			<h3 class="text-lg font-semibold text-gray-800 dark:text-white/90">የየወሩ እስረኞቺ</h3>
+			<h3 class="text-lg font-semibold text-gray-800 dark:text-white/90">Crime Count</h3>
 
 			<div class="relative h-fit">
 
@@ -11,7 +11,7 @@
 
 		<div class="max-w-full overflow-x-auto custom-scrollbar">
 			<div id="chartOne" class="-ml-5 min-w-[650px] xl:min-w-full pl-2">
-				<VueApexCharts v-if="keys.length > 0" type="bar" height="280" :options="chartOptions" :series="series" />
+				<VueApexCharts v-if="keys.length > 0" type="bar" height="380" :options="chartOptions" :series="series" />
 			</div>
 		</div>
 	</div>
@@ -33,12 +33,12 @@ const props = defineProps({
 	}
 })
 
-const values = computed(() => props.dashboardData.monthly_arrests.valuess ?? [])
-const keys = computed(() => props.dashboardData.monthly_arrests.keyss ?? [])
+const values = computed(() => props.dashboardData.crime_counts.valuess ?? [])
+const keys = computed(() => props.dashboardData.crime_counts.keyss ?? [])
 
 const series = computed(() => [
 	{
-		name: 'Monthly Arrests',
+		name: 'Crimes',
 		data: values.value,
 	},
 ]);
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,15 +2,19 @@
 	<admin-layout>
 		<div class="grid grid-cols-12 gap-4 md:gap-6">
 			<div class="col-span-12 space-y-6 xl:col-span-7">
-				<ecommerce-metrics :dashboardData="dashboardData"/>
-				<monthly-target />
+				<ecommerce-metrics v-if="loaded" :dashboardData="dashboardData"/>
+				<monthly-sale v-if="loaded" :dashboardData="dashboardData"/>
 			</div>
 			<div class="col-span-12 xl:col-span-5">
-				<monthly-sale />
+				<monthly-target />
 			</div>
 
+			
 			<div class="col-span-12">
 				<statistics-chart />
+			</div>
+			<div class="col-span-12">
+				<CrimeCount v-if="loaded":dashboardData="dashboardData" />
 			</div>
 		</div>
 	</admin-layout>
@@ -19,8 +23,10 @@
 <script setup>
 import AdminLayout from '../components/layout/AdminLayout.vue'
 import EcommerceMetrics from '../components/ecommerce/EcommerceMetrics.vue'
-import MonthlyTarget from '../components/ecommerce/MonthlySale.vue'
-import MonthlySale from '../components/ecommerce/MonthlyTarget.vue'
+import MonthlyTarget from '../components/ecommerce/MonthlyTarget.vue'
+import MonthlySale from '../components/ecommerce/MonthlySale.vue'
+import CrimeCount from '../components/ecommerce/CrimeCount.vue'
+
 import axios from 'axios'
 
 import { useStore } from 'vuex';
@@ -30,6 +36,7 @@ const store = useStore();
 const dashboardData = ref([])
 const apiServer = computed(() => store.state.apiServer);
 
+const loaded = computed(() => Object.keys(dashboardData.value).length > 0)
 const fetchDashboard = () => {
 	axios
 		.get(apiServer.value + 'dashboard')
